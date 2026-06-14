3af19fc8141c08ebcf24376c9ab20a32a0b6a0ee
Grace Humer <ghumer@sfu.ca>
2026-06-14 16:31:35 -0700

    lab 4 - complete

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -1,6 +1,8 @@
 
 #define _DEFAULT_SOURCE
+#define __POSIX_C_SOURCE 200809L
 
+#include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
@@ -8,12 +10,9 @@
 
 /* Lab 4 - main file
  *
- * Grace Humer, CMPT201 Sumer 2026
+ * Grace Humer, CMPT201 Summer 2026
  */
 
-// DEFINE PROTOTYPES HERE
-
-// DEFINE STRUCTS/ETC
 typedef struct header { // size of WHOLE header is 16 bytes (8 for size, 8 for ptr)
   uint64_t size;
   struct header *next;
@@ -21,41 +20,119 @@ typedef struct header { // size of WHOLE header is 16 bytes (8 for size, 8 for p
 
 #define HEADER_SIZE sizeof(struct header);
 
+struct header *fill_header(struct header *header, uint64_t block_size, struct header *block_next);
+struct header *fill_data(struct header *header, int fill, size_t mem_size);
+struct header *fill_block(struct header *header, uint64_t block_size, struct header *block_next,
+                          int fill, size_t mem_size);
+void print_header(struct header *header);
+void print_data(struct header *header);
+void print_block(struct header *header);
+void print_out(char *format, void *data, size_t data_size);
+
 // MAIN PROGRAM
 int main() {
   const intptr_t heapIncrease = 256;                    // 256 bytes
-  const uint64_t block_size = 128;                      // each block size total
+  const uint64_t block_size = heapIncrease / 2;         // each block size total
   const uint64_t header_size = sizeof(struct header);   // header size
   const uint64_t actual_mem = block_size - header_size; // actual size of free memory
 
-  printf("Header size in bytes: %lu\n", sizeof(struct header));
+  // struct header *b1_data;
+  // struct header *b2_data;
 
   void *program_break = sbrk(0);
-  printf("initial Program Break: %p\n", program_break);
-  // Assume we are given our additional bytes if returns previous address. This address is START of
-  // new memory block
   void *new_program_break = sbrk(heapIncrease);
-  printf("Memory added at address: %p\n", new_program_break);
+  assert(program_break == new_program_break);
 
-  struct header *block_one = (struct header *)new_program_break;
-  block_one->size = block_size;
-  block_one->next = NULL;
+  // print_out("program break (top of heap): %p\n", &program_break, sizeof(program_break));
 
-  // print header
-  // set memory using memset()
-  // reprint to verify no corruption
+  // struct header *block_one = fill_header(program_break, block_size, NULL);
+  // struct header *block_two = fill_header(program_break + block_size, block_size, program_break);
 
-  printf("HEADER: Size = %lu, Next = %p\n", block_one->size, block_one->next);
+  // print_header(block_one);
+  // print_header(block_two);
 
-  void *mem_block_one = new_program_break + header_size;
-  void *return_block_one = memset(mem_block_one, 0, actual_mem);
-  printf("%p\n%p", mem_block_one, return_block_one);
-  printf("HEADer: Size = %lu, Next = %p\n", block_one->size, block_one->next);
-  struct header *block_two = (struct header *)(new_program_break + block_size);
-  block_two->size = block_size;
-  block_two->next = new_program_break;
+  // b2_data = fill_data(block_two, 11, actual_mem);
+  // b2_data = fill_data(block_two, 11, actual_mem);
+
+  // print_header(block_one);
+  // print_header(block_two);
+
+  // print_data(b1_data);
+  // print_data(b2_data);
+
+  // print_block(block_one);
+  // print_block(block_two);
+
+  struct header *block_one =
+      fill_block((struct header *)new_program_break, block_size, NULL, 0, actual_mem);
+  struct header *block_two = fill_block((struct header *)(new_program_break + block_size),
+                                        block_size, new_program_break, 1, actual_mem);
+
+  print_block(block_one);
+  print_block(block_two);
 
   return 0;
 }
 
 // FUNCTION IMPLEMENTATIONS HERE
+
+// header is ptr to entire memory block, size is the size of the memory block (header + actual free)
+// next is the ptr to the 'next' block of memory - NULL if bottom.
+struct header *fill_header(struct header *header, uint64_t block_size, struct header *block_next) {
+  header->size = block_size;
+  header->next = block_next;
+  return header;
+}
+
+// Fills data block of size mem_size with value of fill (0 or 1)
+struct header *fill_data(struct header *header, int fill, size_t mem_size) {
+  void *data = (void *)header + sizeof(struct header);
+  memset(data, fill, mem_size);
+  return header;
+}
+// Fills entire memory block.
+struct header *fill_block(struct header *header, uint64_t block_size, struct header *block_next,
+                          int fill, size_t mem_size) {
+  header = fill_header(header, block_size, block_next);
+  header = fill_data(header, fill, mem_size);
+  return header;
+}
+
+// Prints header values: size and ptr to next free memory, NULL if bottom
+void print_header(struct header *header) {
+  char *str = "\nHEADER: ";
+  write(1, str, strlen(str));
+  print_out("\tcurrent address: %p\n", &header, sizeof(struct header));
+  print_out("\tsize: \t%d\n", &(header->size), sizeof(uint64_t));
+  print_out("\tnext: \t%p\n", &(header->next), sizeof(struct header *));
+}
+
+// Prints data associated with a given header. should already be intitalized to be 0 or 1.
+void print_data(struct header *header) {
+  char *str = "\nDATA: ";
+  write(1, str, strlen(str));
+  void *data = (void *)header + sizeof(struct header);
+  uint64_t memory = (header->size) - sizeof(struct header);
+  for (uint64_t i = 0; i < memory - 4; i++) {
+    print_out("%x\n", data, 1); // Could not get the final result to print correctly :( unsure which
+                                // data type to cast to since I assume we want a direct
+                                // representation of the value, and not a if/else to print the
+                                // correct value :)
+  }
+}
+
+// Prints the block of memory (header + data)
+void print_block(struct header *header) {
+  print_header(header);
+  print_data(header);
+}
+
+void print_out(char *format, void *data, size_t data_size) {
+  char buf[BUFSIZ];
+  ssize_t len = snprintf(buf, BUFSIZ, format,
+                         data_size == sizeof(uint64_t) ? *(uint64_t *)data : *(void **)data);
+  if (len < 0) {
+    perror("snprintf");
+  }
+  write(1, buf, len);
+}
